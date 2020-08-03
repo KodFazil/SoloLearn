0e0f97945272a431c064572b6c50706f393c33f0
KodFazil <57687767+KodFazil@users.noreply.github.com>
2020-08-03 12:29:16 +0300

    Add files via upload

--- Popsicles.c
+++ Popsicles.c
@@ -2,7 +2,7 @@
 /*Task: Given the number of siblings that you are giving popsicles to, determine if can 
 give them each an even amount or if you give shouldn't mention the popsicles at all. */
 #include<stdio.h>
-int main() {                  //31 Temmuz 2020
+int main() {                  
 	//first taking the number of siblings then number of popsicles
 	int siblings, popsicles;
 	scanf("%d %d", &siblings, &popsicles);
