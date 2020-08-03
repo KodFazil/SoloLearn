85e7885c6db78460d47501c5967c7986892dc6dc
KodFazil <57687767+KodFazil@users.noreply.github.com>
2020-08-03 12:29:50 +0300

    Add files via upload

--- Hovercraft.c
+++ Hovercraft.c
@@ -2,7 +2,7 @@
 /*Task: Determine whether or not you made a profit based on how many of the ten 
 hovercrafts you were able to sell that month */
 #include<stdio.h>
-int main() {         // 31 Temmuz 2020 < Fazýl >	
+int main() {         	
 	//10 hovercrafts and insurance total: 21.000.000, when sale 3.000.000
 	//sales of this month
 	int sales, income=0, expense=21000000, piece=3000000;  
