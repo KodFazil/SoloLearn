835fd066c8d33a42f645d7efe02aee26a676aa3b
KodFazil <57687767+KodFazil@users.noreply.github.com>
2020-08-03 12:27:35 +0300

    Add files via upload

--- Deja Vu.c
+++ Deja Vu.c
@@ -2,7 +2,7 @@
 /*Task:If you given a string of random letters, your task is to evaluate whether
 any letter is repeated in the string or if you only hit unique keys while you typing. */
 #include<stdio.h>      
-int main() {                    
+int main() {               
 	//taking a string array
 	char array[10];
 	int i, j;
